d36c38c8ebb87caa275e6b6a173cc3b8c6cae881
jrauber <jrauber@users.noreply.github.com>
2017-11-24 11:42:06 +0100

    initial checkin

---
+++ WebSocketAppBackend/Program.cs
@@ -0,0 +1,16 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+
+namespace WebSocketAppBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new ServerProcess();
+
+            proc.Start("ws://127.0.0.1:8181");
+        }
+    }
+}
---
+++ WebSocketAppBackend/ServerProcess.cs
@@ -0,0 +1,46 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+
+namespace WebSocketAppBackend
+{
+    public class ServerProcess
+    {
+        public void Start(string location)
+        {
+            List<IWebSocketConnection> sockets = new List<IWebSocketConnection>();
+            WebSocketServer server = new WebSocketServer(location);
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("CONNECTION OPENED BY CLIENT");
+                    sockets.Add(socket);
+                };
+
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("CONNECTION CLOSED BY CLIENT");
+                    sockets.Remove(socket);
+                };
+
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine($"RX FROM CLIENT: {message}");
+                    sockets.ForEach(s => s.Send($"OTHER CLIENT SAYS: {message}"));
+                };     
+            });
+
+            Console.WriteLine("TYPE CLOSE TO EXIT AND PRESS ENTER");
+
+            string input = Console.ReadLine();
+
+            while (input != "CLOSE")
+            {
+                sockets.ForEach(s => s.Send($"SERVER SAYS: {input}"));
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
